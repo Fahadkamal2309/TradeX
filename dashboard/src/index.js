9e9534ffd23dc59fb852afb73e67f27f226c51e0
Mohammad Fahad Kamal <8112fahadkamal@gmail.com>
2025-12-06 18:59:23 +0530

    first commit

---
+++ dashboard/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ReactDOM from 'react-dom/client';
+import Home from "./component/Home";
+import './index.css';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+   <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  
+);
+
+
